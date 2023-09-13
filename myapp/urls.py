355a0c1d14ea68e5aa144f2b59c9d6a82f2cba4d
kishanpachani <kp21043@gmail.com>
2023-09-13 15:00:40 +0530

    first commit

---
+++ myapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path,include
+from . import views
+
+urlpatterns = [
+   path('',views.index),
+   path('getalldata/',views.getalldata),
+   path('updatedata/<int:id>',views.updatedata),
+   path('getid/<int:id>',views.getid),
+   path('deleteid/<int:id>',views.deleteid),
+   path('postdata/',views.insertdata),
+]
